bac22cd44d42e625ed5bf09a095766394d6b38ba
mayanxoni <mayanksoni282@gmail.com>
2018-12-01 15:18:28 +0530

    Added some files

--- let.sh
+++ let.sh
@@ -1,15 +1,6 @@
-let a=5+4
-echo $a
-
-let "a = 5 + 4"
-echo $a
-
-let a++
-echo $a
-
-let "a = 4 * 5"
-echo $a
-
+let a=5+4;echo $a
+let "a = 5 + 4";echo $a
+let a++;echo $a
+let "a = 4 * 5";echo $a
 x=1
-let "a = $x + 30"
-echo $a
+let "a = $x + 30";echo $a
